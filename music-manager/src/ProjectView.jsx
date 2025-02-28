1df00ad4e02593e371cab76d1b53d5dd18997993
Mason Wang <wangyachenmason@163.com>
2025-02-28 01:06:08 -0800

    update projectview and songcard UI

--- music-manager/src/ProjectView.jsx
+++ music-manager/src/ProjectView.jsx
@@ -10,11 +10,11 @@ function ProjectView() {
                     <p className="text-slate-50 font-semibold text-2xl">Project View</p>
                 </div>
 
-                <div className="flex flex-row gap-15">
+                <div className="flex flex-row gap-10 w-full">
                     {/* Left-Aligned Content Box */}
-                    <div className="flex flex-col items-start w-full">
+                    <div className="flex flex-col items-start w-1/3">
                         <h2 className="font-medium mb-1 text-slate-50">Project Cover</h2>
-                        <img className="max-w-80 mb-4" src={cover1} alt="project-cover" />
+                        <img className="max-w-80 mb-4 mt-2" src={cover1} alt="project-cover" />
 
                         <h2 className="font-medium mb-1 text-slate-50">Project Name</h2>
                         <p className="font-light mb-4 text-slate-50">Legends Never Die</p>
@@ -23,19 +23,35 @@ function ProjectView() {
                         <p className="font-light mb-4 text-slate-50">Album/EP</p>
 
                         <h2 className="font-medium mb-1 text-slate-50">Project Description</h2>
-                        <p className="font-light mb-4 text-slate-50">jjjjjdi</p>
-                        
+                        <p className="font-light mb-4 text-slate-50 break-words text-left max-w-full">
+                            Legends Never Die is a powerful tribute to Juice WRLD’s legacy, blending raw emotion with his signature melodic flow. Released posthumously, the album dives deep into themes of love, heartbreak, mental health, and perseverance, offering an intimate look into his thoughts and struggles. With heartfelt lyrics and immersive production, Legends Never Die cements Juice WRLD’s place as a voice for a generation, proving that his impact will live on forever.
+                        </p>
+
                         <h2 className="font-medium mb-1 text-slate-50">Project Status</h2>
                         <p className="font-light mb-4 text-slate-50">In Progress</p>
                     </div>
 
-                    <div className="flex flex-col items-start w-full gap-2">
+                    {/* Right-Aligned Content (Songs) */}
+                    <div className="flex flex-col items-start w-2/3 gap-2">
+                        <div className="flex justify-between w-full items-center rounded-lg">
+                            <h2 className="font-semibold text-white">Song List</h2>
+                            <button className="text-white text-sm font-medium rounded-md transition duration-200 p-1 hover:bg-amber-50 hover:text-black">
+                                + New Song
+                            </button>
+                        </div>
+
+                
+                        <SongCard />
+                        <SongCard />
+                        <SongCard />
+                        <SongCard />
                         <SongCard />
                         <SongCard />
                         <SongCard />
                         <SongCard />
                     </div>
                 </div>
+
                 
             </section>
         </>
--- music-manager/src/SongCard.jsx
+++ music-manager/src/SongCard.jsx
@@ -3,13 +3,18 @@ import headphoneIcon from "./assets/headphone.png"
 function SongCard(){
     return(
         <>
-            <div className="flex flex-row items-center w-3xl p-3 justify-between border-1 border-slate-50 rounded-2xl">
-                <img className="bg-slate-50 size-8" src={headphoneIcon} alt="headphone-icon" />
+            <div className="flex flex-row items-center w-full h-20 p-3 justify-between border-1 border-slate-50 rounded-2xl">
+                <button>
+                    <img className="bg-slate-50 p-1 size-8 rounded-2xl transition duration-200 hover:border-2" src={headphoneIcon} alt="headphone-icon" />
+                </button>
                 <p>Song Name</p>
                 <p>00:00</p>
                 <p>Feat.Lil Nas X</p>
-                <button className="font-light border-1 border-slate-50 text-slate-50 py-1 px-2.5 rounded-md hover:bg-slate-50 hover:text-slate-950">Edit</button>
-                <button className="font-light border-1 border-slate-50 text-slate-50 py-1 px-2.5 rounded-md hover:bg-slate-50 hover:text-slate-950">Writing Mode</button>
+                <div className="flex flex-ro gap-1.5">
+                    <button className="font-light border-1 border-slate-50 text-slate-50 py-1 px-2.5 rounded-md transition duration-200 hover:bg-slate-50 hover:text-slate-950">Edit</button>
+                    <button className="font-light border-1 border-slate-50 text-slate-50 py-1 px-2.5 rounded-md transition duration-200 hover:bg-slate-50 hover:text-slate-950">Writing Mode</button>
+                </div>
+                
             </div>
         </>
     );
