33f5d54e8763e1a8ad07753f364250b76a1cd69a
Mason Wang <wangyachenmason@163.com>
2025-03-24 17:21:06 -0700

    fix tailwind config bug for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcssPostcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcssPostcss,
+        autoprefixer
+      ],
+    },
+  },
   server: {
     proxy: {
       '/api': {
